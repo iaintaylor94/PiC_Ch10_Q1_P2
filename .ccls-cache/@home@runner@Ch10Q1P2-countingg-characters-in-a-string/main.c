9f0b5890aaf711d06e579759557a0dcbb2fa3978
Iain Taylor <iain.taylor.94@gmail.com>
2023-08-19 12:35:20 +0000

    copied program

--- .ccls-cache/@home@runner@Ch10Q1P2-countingg-characters-in-a-string/main.c
+++ .ccls-cache/@home@runner@Ch10Q1P2-countingg-characters-in-a-string/main.c
@@ -1,6 +1,28 @@
+// This program counts the number of characters in a string
+
 #include <stdio.h>
 
+// Declare Functions
+int stringLength (const char []);
+
 int main(void) {
-  printf("Hello World\n");
+
+  const char word1 [] = { 'a', 's','t', 'e', 'r', '\0' };
+  const char word2 [] = { 'a', 't', '\0' };
+  const char word3 [] = { 'a', 'w', 'e', '\0' };
+
+  printf ("%d %d %d\n", stringLength(word1), stringLength(word2), stringLength(word3));
+  
   return 0;
+}
+
+// Define Functions
+int stringLength (const char string []) {
+  
+  int count = 0;
+  while (string [count] != '\0') {
+    ++count;
+  }
+
+  return count;
 }
